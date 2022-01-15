f4daa91ed0c2cab6a7588a501183c77443cf2d21
Nnookk <naokiogata@NaokinoMacBook-Air.local>
2022-01-14 20:24:48 -0500

    first commit

---
+++ master.php
@@ -0,0 +1,27 @@
+<?php
+    function url_maker() {
+        
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <link rel="stylesheet" href="./stylesheet/master.css">
+    <title>master.php</title>
+</head>
+<body>
+    <div class="nav">
+        <ul>
+            <li><a href="">Image Upload</a></li>
+            <li><a href="">PDF Upload</a></li>
+        </ul>
+    </div>
+<?php
+    include($_GET['addr']==$addr);
+?>
+</body>
+</html>
